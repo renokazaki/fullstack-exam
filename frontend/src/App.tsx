6977dbcb6729d204a77cfc7a04f38ed6543fa652
ren <ren84690@gmail.com>
2025-09-09 11:45:39 +0900

    frontend-feature:create-PublishAnswerPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import TopPage from "./pages/TopPage";
 import PreviewPage from "./pages/PreviewPage";
 import PublishQuestionPage from "./pages/PublishQuestionPage";
+import PublishAnswerPage from "./pages/PublishAnswerPage";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/top" element={<TopPage />} />
         <Route path="/publishQuestion" element={<PublishQuestionPage />} />
+        <Route path="/publishAnswer" element={<PublishAnswerPage />} />
         <Route path="/preview" element={<PreviewPage />} />
       </Routes>
     </>
