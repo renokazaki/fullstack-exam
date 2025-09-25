b1689bebcf8d82e2e9d24a6d6e63dcd7cfc0346e
ren <ren84690@gmail.com>
2025-09-25 16:35:03 +0900

    fix/deploy-error

--- src/app/Hooks/auth.ts
+++ src/app/Hooks/auth.ts
@@ -1,7 +1,7 @@
 import { createClient } from "../../../utils/supabase/server";
-const supabase = await createClient();
 
 export const getAuth = async () => {
+  const supabase = await createClient();
   const { data, error } = await supabase.auth.getUser();
 
   return { data, error };
